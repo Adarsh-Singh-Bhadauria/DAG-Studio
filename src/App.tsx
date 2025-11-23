b23c95000f1e028ce2b62242af30f5362edf51ff
Adarsh-Singh-Bhadauria <adarshsbhadauria@gmail.com>
2025-11-24 03:12:26 +0530

    removed redundant code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import ReactFlowWrapper from './components/ReactFlowWrapper'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
      <div className="w-full h-screen bg-gray-50">
       <ReactFlowWrapper />
